d9edb80daea0558c8bd1f9f2e63d5fe57e6b5624
Evan Taft <evtaft22@gmail.com>
2020-12-14 12:52:04 -0600

    (add)migrate files

---
+++ covid-19-tracker/src/LineGraph.js
@@ -0,0 +1,121 @@
+import React, { useState, useEffect } from "react";
+import { Line } from "react-chartjs-2";
+import numeral from "numeral";
+
+const caseTypeColor = {
+  deaths: {
+    background: "rgba(204, 16, 52, 0.5)",
+    border: "#CC1034",
+  },
+  recovered: {
+    background: "rgba(144, 238, 144, 0.5)",
+    border: "#90ee90",
+  },
+  cases: {
+    background: "rgba(255, 238, 0, 0.5)",
+    border: "#ffee00",
+  },
+};
+
+const options = {
+  legend: {
+    display: false,
+  },
+  elements: {
+    point: {
+      radius: 0,
+    },
+  },
+  maintainAspectRatio: false,
+  tooltips: {
+    mode: "index",
+    intersect: false,
+    callbacks: {
+      label: function (tooltipItem, data) {
+        return numeral(tooltipItem.value).format("+0,0");
+      },
+    },
+  },
+  scales: {
+    xAxes: [
+      {
+        type: "time",
+        time: {
+          parser: "MM/DD/YY",
+          tooltipFormat: "ll",
+        },
+      },
+    ],
+    yAxes: [
+      {
+        gridLines: {
+          display: false,
+        },
+        ticks: {
+          // Include a dollar sign in the ticks
+          callback: function (value, index, values) {
+            return numeral(value).format("0a");
+          },
+        },
+      },
+    ],
+  },
+};
+
+const buildChartData = (data, casesType) => {
+  let chartData = [];
+  let lastDataPoint;
+  for (let date in data.cases) {
+    if (lastDataPoint) {
+      let newDataPoint = {
+        x: date,
+        y: data[casesType][date] - lastDataPoint,
+      };
+      chartData.push(newDataPoint);
+    }
+    lastDataPoint = data[casesType][date];
+  }
+  return chartData;
+};
+
+function LineGraph({ casesType, className, dateQuery }) {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await fetch(`https://disease.sh/v3/covid-19/historical/all?lastdays=${dateQuery}`)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          let chartData = buildChartData(data, casesType);
+          setData(chartData);
+        });
+    };
+
+    fetchData();
+  }, [casesType, dateQuery]);
+
+
+
+  return (
+    <div className={className}>
+      {data?.length > 0 && (
+        <Line
+          data={{
+            datasets: [
+              {
+                backgroundColor: `${caseTypeColor[casesType]["background"]}`,
+                borderColor: `${caseTypeColor[casesType]["border"]}`,
+                data: data,
+              },
+            ],
+          }}
+          options={options}
+        />
+      )}
+    </div>
+  );
+}
+
+export default LineGraph;
